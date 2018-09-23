f649ade82ae277d1aefe8291b4e28d8a5a96beaa
Mark W <markwuu@gmail.com>
2018-09-22 15:18:15 -1000

    changed tiles orientation

--- src/App.js
+++ src/App.js
@@ -1,31 +1,29 @@
-import React, { Component } from 'react';
-import './App.css';
-import { render } from 'react-dom'
-import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
+import React, { Component } from "react";
+import "./App.css";
+import { render } from "react-dom";
+import { Map, Marker, Popup, TileLayer } from "react-leaflet";
 
-const ReactLeaflet = { Map: Map, TileLayer};
-
+const ReactLeaflet = { Map: Map, TileLayer };
 
 class App extends React.Component {
   constructor() {
-    super()
+    super();
     this.state = {
       lat: 21.2899317,
-      lng: -157.8525660,
+      lng: -157.852566,
       zoom: 13
-    }
+    };
   }
 
   render() {
     const position = [this.state.lat, this.state.lng];
     return (
       <Map center={position} zoom={this.state.zoom}>
-        <TileLayer url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'/>
+        <TileLayer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png" />
       </Map>
     );
   }
 }
 
-
-render(<App />, document.getElementById('container'))
+render(<App />, document.getElementById("container"));
 export default App;
